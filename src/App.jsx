0fbeacab3bb4db8e42f66bd7a4b7412a82efd2b8
HimAYouN <himayoun7931@gmail.com>
2024-04-23 21:54:11 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import {appwriteUrl} from './conf/conf'
+
+function App() {
+
+  console.log(appwriteUrl)
+  return (
+    <>
+      <h1>A blog app with appwrite</h1>
+    </>
+  )
+}
+
+export default App

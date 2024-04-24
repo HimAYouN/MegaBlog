4bf5623d7b3d3d7f1469b912fcfe412fdcf98f09
HimAYouN <himayoun7931@gmail.com>
2024-04-24 11:50:42 +0530

    fixex

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,8 @@ import { useDispatch } from 'react-redux'
 import authService from './appwrite/auth'
 import {login, logout} from './store/authSlice'
 import './App.css'
+import { Footer, Header } from './components'
+import { Outlet } from 'react-router-dom'
 
 function App() {
   const [loading, setLoading] = useState(true)
@@ -21,9 +23,17 @@ function App() {
     .finally( () =>{setLoading(false)})
   }, [])
 
-  
+
   return !loading ? (
-    <div></div>
+    <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+      <div className='w-full block'>
+        <Header />
+        <main>
+           TODO: {/* <Outlet /> */}
+        </main>
+        <Footer />
+      </div>
+    </div>
   ) : null 
   
 }

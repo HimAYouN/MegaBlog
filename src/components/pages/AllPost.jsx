ba51452f48e017d7222834e3e442dec1fe9c3cb9
HimAYouN <himayoun7931@gmail.com>
2024-04-26 11:12:09 +0530

    added pages + routing

---
+++ src/components/pages/AllPost.jsx
@@ -0,0 +1,32 @@
+import React, {useState, useEffect} from 'react'
+import {appwriteService} from '../../appwrite/config'
+import { Container, PostCard } from '../index'
+
+function AllPost() {
+    const [post, setPost] = useState([])
+    useEffect(()=>{
+        appwriteService.getPosts([]).then((posts) => {
+            if(posts){
+                setPost(posts.documents)
+            }
+        })
+    }, [])
+  return (
+    <div className='w-full py-8'>
+        <Container>
+            <div className='flex flex-wrap'>
+                {
+                    post.map((post)=>(
+                        <div key={post.$id} className='p-2 w-1/4'>
+                            <PostCard />
+                        </div>
+
+                    ))
+                }
+            </div>
+        </Container>
+    </div>
+  )
+}
+
+export default AllPost
--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,81 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import { Provider } from 'react-redux'
-import store from './store/store.js'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { Provider } from "react-redux";
+import store from "./store/store.js";
+import "./index.css";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { AuthLayout, Login,  } from "./components/index.js";
+import Home from "./components/pages/Home.jsx";
+import Signup from './components/pages/Signup.jsx'
+import AllPost from './components/pages/AllPost.jsx'
+import AddPost from './components/pages/AddPost.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/login",
+        element: (
+          <AuthLayout authentication={false}>
+            <Login />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/signup",
+        element: (
+          <AuthLayout authentication={false}>
+            <Signup />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/all-posts",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AllPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/add-post",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <AddPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/edit-post/:slug",
+        element: (
+          <AuthLayout authentication>
+            {" "}
+            <EditPost />
+          </AuthLayout>
+        ),
+      },
+      {
+        path: "/post/:slug",
+        element: <Post />,
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+      <RouterProvider router={router}/>
     </Provider>
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
